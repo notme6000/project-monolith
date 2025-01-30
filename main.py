bb98efb5161e551077716019e22e2693dd3bd128
notme6000 <sureshjithu9400@gmail.com>
2025-01-30 21:26:09 +0530

    ui fixing

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-import subprocess
+import webview
 
-subprocess.run("gnome-terminal")
+# Create the PyWebView window
+window = webview.create_window('Project FRANXX', 'web-pen.html',
+                               width=1200,
+                               height=700,
+                               resizable= False
+                            )
+
+# Start a thread to load the main page after 5 seconds
+
+
+# Start the PyWebView event loop
+webview.start()
