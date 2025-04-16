898d63d2aa39d87d48020b804df89f97855d3f62
notme6000 <sureshjithu9400@gmail.com>
2025-04-16 23:05:36 +0530

    edited setup.sh file

--- installation/setup.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Set an environment variable
-set MY_VARIABLE=HelloWorld
-
-REM Print the value of the environment variable
-echo %MY_VARIABLE%
-
-REM Pause the script to keep the window open
-pause
---
+++ installation/setup.sh
@@ -0,0 +1,3 @@
+pip install -r requirement.txt
+
+sudo apt install nmap amass whois gobuster crunch sherlock 
